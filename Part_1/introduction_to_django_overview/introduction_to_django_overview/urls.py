ea7dc05941c23d079bea1b4081f5d8e105644ae4
Muhammed Songur <songurtechnology@gmail.com>
2023-10-04 08:07:16 +0300

    Your commit message here

--- Part_1/introduction_to_django_overview/introduction_to_django_overview/urls.py
+++ Part_1/introduction_to_django_overview/introduction_to_django_overview/urls.py
@@ -4,7 +4,9 @@ from introduction_to_django_overview_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("post/<int:year>/", views.year_archive, name="year_archive"),
-    path("post/<int:year>/<int:month>/", views.month_archive, name="month_archive"),
-    path("post/<int:year>/<int:month>/<int:pk>/", views.post_detail, name="post_detail"),
+    path("archive/<int:year>/", views.year_archive, name="year_archive"),
+    path("archive/<int:year>/<int:month>/", views.month_archive, name="month_archive"),
+    path("archive/<int:year>/<int:month>/<int:pk>/", views.post_detail, name="post_detail"),
 ]
+
+
--- Part_1/introduction_to_django_overview/introduction_to_django_overview_app/views.py
+++ Part_1/introduction_to_django_overview/introduction_to_django_overview_app/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def year_archive(request, year):
-    p_list = Product.objects.filter(share_date__year=year)
-    relation = {"year": year, "post_list": p_list}
-    return render(request, "introduction_to_django_overview_app/year_archive.html", relation)
+    post_list = Product.objects.filter(share_date__year=year)
+    return render(request, "year_archive.html", {"year": year, "post_list": post_list})
 
 def month_archive(request, year, month):
-    p_list = Product.objects.filter(share_date__year=year, share_date__month=month)
-    relation = {"year": year, "month": month, "post_list": p_list}
-    return render(request, "introduction_to_django_overview_app/month_archive.html", relation)
-
+    post_list = Product.objects.filter(share_date__year=year, share_date__month=month)
+    return render(request, "month_archive.html", {"year": year, "month": month, "post_list": post_list})
 
 def post_detail(request, year, month, pk):
     post = get_object_or_404(Product, share_date__year=year, share_date__month=month, pk=pk)
-    return render(request, "introduction_to_django_overview_app/post_detail.html", {"post": post})
+    return render(request, "post_detail.html", {"post": post})
